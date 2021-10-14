a6bfc5e0b10d42b7cbb0176ed633af699b8ccb05
Harlan Wilton <harlan@harlanzw.com>
2021-10-14 23:10:10 +1100

    fix: use eslint-plugin-jest

--- packages/nuxt-windicss/src/module.ts
+++ packages/nuxt-windicss/src/module.ts
@@ -83,11 +83,11 @@ const defineNuxtWindiCSSModule = defineNuxtModule<NuxtWindiOptions>(nuxt => ({
     const utils = createUtils(nuxtWindiOptions, { root: nuxtWindiOptions.root, name: NAME })
 
     const ensureInit = utils.init()
-        .then(() => nuxt.callHook('windicss:utils', utils))
+      .then(() => nuxt.callHook('windicss:utils', utils))
 
     // if the user hasn't manually added virtual:windi.css to their nuxt config then we push it as the first stylesheet
     const windiImports = nuxt.options.css.filter(
-        css => (typeof css === 'string' ? css : css.src).includes('virtual:windi'),
+      css => (typeof css === 'string' ? css : css.src).includes('virtual:windi'),
     )
     if (!windiImports.length)
       nuxt.options.css.unshift('virtual:windi.css')
@@ -105,8 +105,8 @@ const defineNuxtWindiCSSModule = defineNuxtModule<NuxtWindiOptions>(nuxt => ({
        */
       // @ts-ignore
       nuxt.hook('build:templates', (
-          { templateVars, templatesFiles }:
-              { templateVars: { css: ({ src: string; virtual: boolean } | string)[] }; templatesFiles: { src: string }[] },
+        { templateVars, templatesFiles }:
+        { templateVars: { css: ({ src: string; virtual: boolean } | string)[] }; templatesFiles: { src: string }[] },
       ) => {
         // normalise the virtual windi imports
         templateVars.css = templateVars.css.map((css) => {
@@ -121,21 +121,21 @@ const defineNuxtWindiCSSModule = defineNuxtModule<NuxtWindiOptions>(nuxt => ({
         })
         // replace the contents of App.js
         templatesFiles
-            .map((template) => {
-              if (!template.src.endsWith('App.js'))
-                return template
-
-              // we need to replace the App.js template..
-              const file = readFileSync(template.src, { encoding: 'utf-8' })
-              // regex replace the css loader
-              const regex = /(import '<%= )(relativeToBuild\(resolvePath\(c\.src \|\| c, { isStyle: true }\)\))( %>')/gm
-              const subst = '$1c.virtual ? c.src : $2$3'
-              const appTemplate = file.replace(regex, subst)
-              const newPath = join(__dirname, 'template', 'App.js')
-              writeFileSync(newPath, appTemplate)
-              template.src = newPath
+          .map((template) => {
+            if (!template.src.endsWith('App.js'))
               return template
-            })
+
+            // we need to replace the App.js template..
+            const file = readFileSync(template.src, { encoding: 'utf-8' })
+            // regex replace the css loader
+            const regex = /(import '<%= )(relativeToBuild\(resolvePath\(c\.src \|\| c, { isStyle: true }\)\))( %>')/gm
+            const subst = '$1c.virtual ? c.src : $2$3'
+            const appTemplate = file.replace(regex, subst)
+            const newPath = join(__dirname, 'template', 'App.js')
+            writeFileSync(newPath, appTemplate)
+            template.src = newPath
+            return template
+          })
       })
     }
 
