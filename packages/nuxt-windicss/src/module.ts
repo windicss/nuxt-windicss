b08b6448b78758efc31736fc953cc28fc85726c1
Harlan Wilton <harlan@harlanzw.com>
2022-07-28 18:58:22 +1000

    fix: require nuxt ^2.0.0 || ^3.0.0-rc.6

--- packages/nuxt-windicss/src/module.ts
+++ packages/nuxt-windicss/src/module.ts
@@ -59,7 +59,7 @@ export default defineNuxtModule<ModuleOptions>({
     name: 'nuxt-windicss',
     configKey: 'windicss',
     compatibility: {
-      nuxt: '^2.0.0 || ^3.0.0-rc.1',
+      nuxt: '^2.0.0 || ^3.0.0-rc.6',
     },
   },
   defaults: {
