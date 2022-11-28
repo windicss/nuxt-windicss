8e61a438c319802cca43eb5d1bb1f4b5a9807120
Harlan Wilton <harlan@harlanzw.com>
2022-11-28 11:30:47 +1100

    fix: require nuxt v3 stable

--- packages/nuxt-windicss/src/module.ts
+++ packages/nuxt-windicss/src/module.ts
@@ -59,7 +59,7 @@ export default defineNuxtModule<ModuleOptions>({
     name: 'nuxt-windicss',
     configKey: 'windicss',
     compatibility: {
-      nuxt: '^2.0.0 || ^3.0.0-rc.13',
+      nuxt: '^2.0.0 || ^3.0.0',
     },
   },
   defaults(nuxt) {
