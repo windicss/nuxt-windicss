2374fe65f3ae7166edce9315ec4439917bb235c9
Harlan Wilton <harlan@harlanzw.com>
2022-11-28 11:41:52 +1100

    fix(@nuxt/content): only support md file extraction

--- packages/nuxt-windicss/src/runtime/server/class-extractor.mjs
+++ packages/nuxt-windicss/src/runtime/server/class-extractor.mjs
@@ -1,5 +1,6 @@
 import { visit } from 'unist-util-visit'
 import { createUtils } from '@windicss/plugin-utils'
+import { extname } from 'pathe'
 import config from '#windicss/config'
 
 export default async (nitroApp) => {
@@ -9,10 +10,18 @@ export default async (nitroApp) => {
     preflight: false,
     config,
   }, { name: 'nuxt-windicss-nitro' })
-  nitroApp.hooks.hook('content:file:afterParse', async (file) => {
+  nitroApp.hooks.hook('content:file:afterParse', async (content) => {
+    if (!content.body || !Array.isArray(content.body.children))
+      return
+
+    const ext = extname(content.id)
+    // only markdown supported
+    if (!ext.endsWith('md'))
+      return
+
     await utils.ensureInit()
 
-    visit(file.body, n => !!n?.props?.class, (node) => {
+    visit(content.body, n => !!n?.props?.class, (node) => {
       utils.addClasses([
         // @todo attributify support
         ...node.props.class.split(' '),
@@ -23,7 +32,7 @@ export default async (nitroApp) => {
       return
     const css = await utils.generateCSS()
     // add to the end of the file
-    file.body.children.push({
+    content.body.children.push({
       type: 'element',
       tag: 'style',
       props: {
